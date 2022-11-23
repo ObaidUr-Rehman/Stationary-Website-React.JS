c80f55e69fcd824a6ec2b62ed90578d4fa277bf9
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-11-23 18:44:25 +0200

    footer done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Heading from './components/Heading';
 import Journal from './components/Journal';
 import Arrival from './components/Arrival';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
     <Heading></Heading>
     <Journal></Journal>
     <Arrival></Arrival>
+    <Footer></Footer>
     </div>
   )
 }
